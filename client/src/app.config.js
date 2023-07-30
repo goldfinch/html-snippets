c1ad4e11763f1981ecfdb36f8cb2f964bf276e79
Art Karasev <as.karasyov@gmail.com>
2023-07-30 23:49:05 +1200

    init components

---
+++ client/src/app.config.js
@@ -0,0 +1,9 @@
+export default function initCfg(command, mode, ssrBuild) {
+
+  const dev = command === 'serve';
+  const host = 'silverstripe-starter.lh';
+
+  return {
+    sassAdditionalData: ``,
+  }
+}
---
+++ client/src/vite.config.js
@@ -0,0 +1,91 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import autoprefixer from "autoprefixer";
+import * as path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import initCfg from './app.config.js'
+
+export default defineConfig(({ command, mode, ssrBuild }) => {
+
+  const cfg = initCfg(command, mode, ssrBuild)
+
+  // const host = cfg.host;
+
+  // fs.writeFileSync('app.config.json', JSON.stringify(cfg.public));
+
+  return {
+
+    esbuild: {
+      // charset: 'ascii' // fixing issue with unicode HTML entities in content: '';
+    },
+
+    resolve: {
+        alias: {}
+    },
+
+    build: {
+      chunkSizeWarningLimit: 1500,
+      emptyOutDir: true,
+      outDir: '../dist',
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name].js`,
+          chunkFileNames: `js/[name]-[hash].js`,
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name.endsWith('.css')) {
+              return '[name][extname]'
+            } else if (
+              assetInfo.name.match(/(\.(woff2?|eot|ttf|otf)|font\.svg)(\?.*)?$/)
+            ) {
+              return 'fonts/[name][extname]'
+            } else if (assetInfo.name.match(/\.(jpg|png|svg)$/)) {
+              return 'images/[name][extname]'
+            }
+
+            return 'js/[name][extname]'
+          },
+          // manualChunks(id) {
+          //     if (id.includes('node_modules')) {
+          //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          //     }
+          // }
+        }
+      }
+    },
+
+    plugins: [
+
+      laravel({
+        input: [
+          // 'src/bootstrap-icons.scss',
+        ],
+        refresh: true,
+        // buildDirectory: '',
+      }),
+
+      // viteStaticCopy({
+      //   targets: [
+      //     {
+      //       src: './node_modules/bootstrap-icons/font/fonts/*',
+      //       dest: '../dist/bootstrap-icons/fonts',
+      //     },
+      //   ],
+      // })
+    ],
+
+    css: {
+
+      // preprocessorOptions: {
+      //   scss: {
+      //     additionalData: cfg.sassAdditionalData,
+      //   },
+      // },
+      postcss: {
+        plugins: [
+          autoprefixer,
+        ],
+      }
+    },
+  }
+
+});
