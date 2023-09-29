46a31fc41795cadbc11de34aa7be724819e5dcf6
Art Karasev <as.karasyov@gmail.com>
2023-09-29 15:05:26 +1300

    tweaks

--- src/Commands/MakeComponentCommand.php
+++ src/Commands/MakeComponentCommand.php
@@ -5,6 +5,7 @@ namespace App\Commands;
 use Goldfinch\Taz\Console\GeneratorCommand;
 use Goldfinch\Taz\Services\InputOutput;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 
 #[AsCommand(name: 'make:component')]
 class MakeComponentCommand extends GeneratorCommand
@@ -24,6 +25,19 @@ class MakeComponentCommand extends GeneratorCommand
     protected function execute($input, $output): int
     {
         parent::execute($input, $output);
+
+        $nameInput = $this->getAttrName($input);
+
+        // Nest template
+
+        $command = $this->getApplication()->find('make:component-template');
+
+        $arguments = [
+            'name' => $nameInput,
+        ];
+
+        $greetInput = new ArrayInput($arguments);
+        $returnCode = $command->run($greetInput, $output);
 
         return Command::SUCCESS;
     }
---
+++ src/Commands/MakeComponentTemplateCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Commands;
+
+use Goldfinch\Taz\Console\GeneratorCommand;
+use Goldfinch\Taz\Services\InputOutput;
+use Symfony\Component\Console\Command\Command;
+
+#[AsCommand(name: 'make:component-template')]
+class MakeComponentTemplateCommand extends GeneratorCommand
+{
+    protected static $defaultName = 'make:component-template';
+
+    protected $description = 'Create new component template';
+
+    protected $path = 'themes/main/templates/Components';
+
+    protected $type = 'componenttemplate';
+
+    protected $stub = './stubs/componenttemplate.stub';
+
+    protected $prefix = 'Component';
+
+    protected function execute($input, $output): int
+    {
+        parent::execute($input, $output);
+
+        return Command::SUCCESS;
+    }
+}
